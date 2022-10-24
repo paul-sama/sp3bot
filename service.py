92ff04aa1f0d906810024846a76d3aacee57c8e0
Paul <anyeccc@gmail.com>
2022-10-24 11:19:47 +0800

    logger rotation

--- service.py
+++ service.py
@@ -6,8 +6,8 @@ from sp3bot.bot import main
 from loguru import logger
 
 logger.configure(handlers=[{"sink": sys.stderr, "level": "INFO"}])
-logger.add("sp3bot.log", level="DEBUG", filter=lambda record: "cron" not in record["extra"])
-logger.add("cron_job.log", filter=lambda record: "cron" in record["extra"])
+logger.add("logs/sp3bot.log", level="DEBUG", filter=lambda record: "cron" not in record["extra"], rotation="08:00")
+logger.add("logs/cron_job.log", filter=lambda record: "cron" in record["extra"], rotation="08:00")
 
 
 if __name__ == '__main__':
