62953bd973082acea8e7c814cf2dbd242a71fa62
Paul <anyeccc@gmail.com>
2022-10-27 09:42:05 +0800

    rotation by file size

--- service.py
+++ service.py
@@ -6,8 +6,8 @@ from sp3bot.bot import main
 from loguru import logger
 
 logger.configure(handlers=[{"sink": sys.stderr, "level": "INFO"}])
-logger.add("logs/sp3bot.log", level="DEBUG", filter=lambda record: "cron" not in record["extra"], rotation="08:00")
-logger.add("logs/cron_job.log", filter=lambda record: "cron" in record["extra"], rotation="08:00")
+logger.add("logs/sp3bot.log", level="DEBUG", filter=lambda record: "cron" not in record["extra"], rotation="500 MB")
+logger.add("logs/cron_job.log", filter=lambda record: "cron" in record["extra"], rotation="500 MB")
 
 
 if __name__ == '__main__':
