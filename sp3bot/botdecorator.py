00d0c52e87890a8b72ebcb7affc26945e5f2abdd
Paul <anyeccc@gmail.com>
2022-11-04 10:12:48 +0800

    log

--- sp3bot/botdecorator.py
+++ sp3bot/botdecorator.py
@@ -44,13 +44,14 @@ def check_session_handler(func):
             user_name = update.effective_user.username
             first_name = update.effective_user.first_name
             last_name = update.effective_user.last_name
-            full_name = f'{first_name} {last_name}'
+            full_name = f'{first_name or ""} {last_name or ""}'
             logger.info(f'{func.__name__:>20}, {user_id}, {user_name}, {full_name} input: {update.message.text}')
             get_or_set_user(user_id=user_id, user_name=user_name, first_name=first_name, last_name=last_name)
             user = get_or_set_user(user_id=user_id)
-            msg = f'Hello {user.first_name}!'
+            msg = f'Hello {user.first_name or full_name}!'
             if not user.session_token:
                 msg += ' please /login'
+                logger.info(msg)
                 await ctx.bot.send_message(chat_id=user_id, text=msg)
                 return
 
@@ -58,4 +59,3 @@ def check_session_handler(func):
         return result
 
     return wrapper
-
