f881d9274391302f0fb16ad965491ea42c5f9c65
Paul <anyeccc@gmail.com>
2022-11-16 18:36:14 +0800

    img

--- sp3bot/media.py
+++ sp3bot/media.py
@@ -17,29 +17,29 @@ except ImportError:
         return None
 
 
-def d_img(img_list):
+def download_img(img_list):
     if not os.path.exists(IMG_DIR):
         os.makedirs(IMG_DIR)
 
-    for n, img in img_list:
-        path = img.split('?')[0]
-        path = path.split('nintendo.net/')[1].rsplit('/', 1)[0]
-        img_name = f'{n}.png'
+    for img_name, img_url in img_list:
+        if not img_url or 'nintendo.net' not in img_url:
+            logger.debug(f'skip wrong img: {img_url}')
+            continue
+        path = img_url.split('?')[0].split('nintendo.net/')[1].rsplit('/', 1)[0]
         img_folder = f'{IMG_DIR}{path}'
         if not os.path.exists(img_folder):
             os.makedirs(img_folder)
-        img_path = f'{IMG_DIR}{path}/{n}.png'
 
+        img_path = f'{img_folder}/{img_name}.png'
         if not os.path.exists(img_path):
-            logger.debug('downloading new file: %s' % img_name)
-            p_res = requests.get(img, stream=True)
+            logger.debug(f'downloading new file: {img_name}.png')
+            p_res = requests.get(img_url, stream=True)
             with open(img_path, 'wb') as pf:
                 shutil.copyfileobj(p_res.raw, pf)
 
 
 def get_img_path(name, url):
-    path = url.split('?')[0]
-    path = path.split('nintendo.net/')[1].rsplit('/', 1)[0]
+    path = url.split('?')[0].split('nintendo.net/')[1].rsplit('/', 1)[0]
     img_path = f'{IMG_DIR}{path}/{name}.png'
     return img_path
 
@@ -49,6 +49,7 @@ def get_stage_img(cur_hour=0):
     res = get_mysql_data('splatoon3', sql)
     if not res:
         return
+
     data = json.loads(res[0]['raw_data'])
     nodes = data['data']['bankaraSchedules']['nodes']
 
@@ -59,39 +60,47 @@ def get_stage_img(cur_hour=0):
 
         path_img_schedule = f"{IMG_DIR}images/schedule/{n['startTime']}.png"
         if os.path.exists(path_img_schedule):
+            logger.debug(f'found schedule img: {path_img_schedule}')
             return path_img_schedule
 
-        c_stages = n['bankaraMatchSettings'][0]['vsStages']
-        c_rule = base64.b64decode(n['bankaraMatchSettings'][0]['vsRule']['id']).decode('utf-8')
-        o_rule = base64.b64decode(n['bankaraMatchSettings'][1]['vsRule']['id']).decode('utf-8')
-        o_stages = n['bankaraMatchSettings'][1]['vsStages']
-        img_stage = get_img_path(c_stages[0]['name'], c_stages[0]['image']['url'])
-        img_stage2 = get_img_path(c_stages[1]['name'], c_stages[1]['image']['url'])
-        img_stage3 = get_img_path(o_stages[0]['name'], o_stages[0]['image']['url'])
-        img_stage4 = get_img_path(o_stages[1]['name'], o_stages[1]['image']['url'])
+        pth_dir = os.path.dirname(path_img_schedule)
+        if not os.path.exists(pth_dir):
+            os.makedirs(pth_dir)
 
-        d_img([(c_stages[0]['name'], c_stages[0]['image']['url']), (c_stages[1]['name'], c_stages[1]['image']['url'])])
-        d_img([(o_stages[0]['name'], o_stages[0]['image']['url']), (o_stages[1]['name'], o_stages[1]['image']['url'])])
+        c_stages = n['bankaraMatchSettings'][0]['vsStages']
+        o_stages = n['bankaraMatchSettings'][1]['vsStages']
+
+        download_img([
+            (c_stages[0]['name'], c_stages[0]['image']['url']), (c_stages[1]['name'], c_stages[1]['image']['url']),
+            (o_stages[0]['name'], o_stages[0]['image']['url']), (o_stages[1]['name'], o_stages[1]['image']['url'])
+        ])
 
         dir_tmp = f'{IMG_DIR}images'
 
         if not os.path.exists(dir_tmp):
             os.makedirs(dir_tmp)
         os.chdir(dir_tmp)
-        call(['convert', '+append', img_stage, img_stage2, 'c.png'])
 
-        call(['convert', '+append', img_stage3, img_stage4, 'o.png'])
+        img_stage1 = get_img_path(c_stages[0]['name'], c_stages[0]['image']['url'])
+        img_stage2 = get_img_path(c_stages[1]['name'], c_stages[1]['image']['url'])
+        img_stage3 = get_img_path(o_stages[0]['name'], o_stages[0]['image']['url'])
+        img_stage4 = get_img_path(o_stages[1]['name'], o_stages[1]['image']['url'])
+        c_rule = base64.b64decode(n['bankaraMatchSettings'][0]['vsRule']['id']).decode('utf-8')
+        o_rule = base64.b64decode(n['bankaraMatchSettings'][1]['vsRule']['id']).decode('utf-8')
 
-        pth_dir = os.path.dirname(path_img_schedule)
-        if not os.path.exists(pth_dir):
-            os.makedirs(pth_dir)
+        for c in [
+            f'convert +append {img_stage1} {img_stage2} c.png',
+            f'convert +append {img_stage3} {img_stage4} o.png',
+            f'convert -append  {c_rule}.png c.png {o_rule}.png o.png {path_img_schedule}'
+        ]:
+            logger.debug(c)
+            os.system(c)
 
-        cmd = f'convert -append  {c_rule}.png c.png {o_rule}.png o.png {path_img_schedule}'
-        logger.debug(cmd)
-        os.system(cmd)
-
-        logger.debug(path_img_schedule)
-        return path_img_schedule
+        if os.path.exists(path_img_schedule):
+            logger.debug(f'set img: {path_img_schedule}')
+            return path_img_schedule
+        else:
+            logger.debug(f'not found path_img_schedule')
 
 
 if __name__ == '__main__':
